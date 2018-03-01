bd26a5e4914cbee9fbf25261ec7c780b520e0f19
Nils Ballmann <nils.ballmann.ext@siemens.com>
2018-03-01 14:52:30 +0100

    main import in package like pip does

--- PACKAGE_NAME/__init__.py
+++ PACKAGE_NAME/__init__.py
@@ -3,9 +3,11 @@
 
 # pylint: disable=C0111
 
-__updated__ = '2018-03-01 14:15:59'
+__updated__ = '2018-03-01 14:51:44'
 
 __version_info__ = [PACKAGE_MAJOR_VERSION, PACKAGE_MINOR_VERSION, PACKAGE_PATCH_VERSION]
 __version__ = '.'.join([str(digit) for digit in __version_info__])
+
+from .main import main
 
 PACKAGE_PUBLIC_API
