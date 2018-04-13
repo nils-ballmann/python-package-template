67012fc9a565a031a5c0986771910b022b9bd9fb
Nils Ballmann <nils.ballmann.ext@siemens.com>
2018-04-13 11:05:22 +0200

    Adding __all__ to __init__ for sphinx

--- PACKAGE_NAME/__init__.py
+++ PACKAGE_NAME/__init__.py
@@ -3,10 +3,14 @@
 
 # pylint: disable=C0111
 
-__updated__ = '2018-03-01 14:51:44'
+__updated__ = '2018-04-13 11:04:46'
 
 __version_info__ = [PACKAGE_MAJOR_VERSION, PACKAGE_MINOR_VERSION, PACKAGE_PATCH_VERSION]
 __version__ = '.'.join([str(digit) for digit in __version_info__])
+
+__all__ = [
+    'PACKAGE_PUBLIC_API'
+]
 
 from .main import main
 
